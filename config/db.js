5e3fd17485bc39d653559f171ea6c0e8dfb12062
Rohit Sasane <rohittsasane@gmail.com>
2025-12-13 23:45:34 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
 import mysql from "mysql2/promise";
 
 // Create MySQL connection pool (Railway variables)
-export const pool = mysql.createPool({
-  host:"mysql.railway.internal",
-  user:"root",
-  password: "trJmYrszBDVhPDSQUaYCcdkQhmESoDWZ",
-  database: "railway",
-  port: "3306",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
 
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10
+});
 // Connect with retry (important on Railway)
 export const connectDB = async (retries = 5) => {
   try {
